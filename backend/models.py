eae18f095ce83193e63190c6a638d7c7ef69ca25
TheeAnon <hazardovic1@gmail.com>
2023-06-08 15:27:57 +0300

    Django-React Linked

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,43 @@
+from typing import Any
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, phone, password):
+        if not email:
+            raise ValueError("Email address cannot be blank")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, phone=phone)
+        user.set_password(password)
+        user.save()
+    
+    def create_admin():
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = models.PhoneNumberField()
+    is_active = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
+
+    def get_first_name(self):
+        return self.first_name
+
+    def get_last_name(self):
+        return self.last_name
+
+    def get_full_name(self):
+        return self.first_name+' '+self.last_name
+
+    def get_phone(self):
+        return self.phone
+
+    def __str__(self):
+        return self.email
